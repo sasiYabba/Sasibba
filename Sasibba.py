7507f3381cd15995a81df854c17e6d4d1d2f5144
sasiYabba <93094359+sasiYabba@users.noreply.github.com>
2021-11-09 11:05:16 +0530

    Update Sasibba.py

--- Sasibba.py
+++ Sasibba.py
@@ -86,7 +86,7 @@ def tech_abm():
     if username =="Real":
         os.system("clear")
         print logo
-        print ("[+] Username :(Correct)")
+        print ("[+] Username :sasiya
         passwordss = raw_input("\033[1;97m[+]\033[1;97m Password :\033[1;97m ")
         if passwordss =="bba":
             os.system("clear")
